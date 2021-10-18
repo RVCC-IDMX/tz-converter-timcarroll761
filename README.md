3f7ed5e14bb5a5d976bafb163dd48ec16b1a9428
timcarroll761 <timcarroll761@gmail.com>
2021-10-17 23:36:12 -0400

    Add yargs and nice formatting

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# template
+# Time zone converter
+
+This program will tell you what time it is in the specified time zone. Usage examples:
+![usage without --format](https://github.com/RVCC-IDMX/tz-converter-timcarroll761/blob/main/Screenshot_30.png?raw=true)
+![usage with --format](https://github.com/RVCC-IDMX/tz-converter-timcarroll761/blob/main/Screenshot_29.pngraw=true)
--- tz.js
+++ tz.js
@@ -1,17 +1,30 @@
 const moment = require('moment-timezone');
+const yargs = require('yargs');
 
 moment.tz.setDefault('America/New_York');
 
-if (process.argv.length !== 3) {
+if (yargs.argv._.length < 1) {
   console.log('Incorrectly formatted querey\nUsage: npm tz.js <timezone>');
   process.exit(1);
 } else {
-  const target = process.argv[2];
-  console.log(
-    `The time here is ${moment()
-      .tz('America/New_York')
-      .format()}\nThe time in ${target
-      .split('/')[1]
-      .replace('_', ' ')} is ${moment().tz(target).format()}`
-  );
+  const target = yargs.argv._[0];
+  if (yargs.argv.format) {
+    console.log(
+      `The time here is ${moment()
+        .tz('America/New_York')
+        .format('dddd, MMMM Do YYYY, h:mm:ss a')}\nThe time in ${target
+        .split('/')[1]
+        .replace('_', ' ')} is ${moment()
+        .tz(target)
+        .format('dddd, MMMM Do YYYY, h:mm:ss a')}`
+    );
+  } else {
+    console.log(
+      `The time here is ${moment()
+        .tz('America/New_York')
+        .format()}\nThe time in ${target
+        .split('/')[1]
+        .replace('_', ' ')} is ${moment().tz(target).format()}`
+    );
+  }
 }
